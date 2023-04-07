fa710f8def2848092c2ff3fde3c139d388493ca8
varvara-chornomorets <vchornomorets@kse.org.ua>
2023-04-07 11:15:56 +0300

    created simple queue + class node
    created queue can't be really used as we need to put nodes in it, i guess, not just what i was putting (keyvalue pair and int)

---
+++ greed-and-encryption/Node.cs
@@ -0,0 +1,25 @@
+﻿namespace greed_and_encryption;
+
+public class Node
+{
+    public Node(int frequency, string character, Node? rightChild, Node? leftchild)
+    {
+        Frequency = frequency;
+        Character = character;
+        RightChild = rightChild;
+        Leftchild = leftchild;
+    }
+
+    public int Frequency { get; set; }
+    public string Character { get; set; }
+    public Node? RightChild { get; set; }
+    public Node? Leftchild { get; set; }
+
+    public Node Merge(Node first, Node second)
+    {
+        return new Node(first.Frequency + second.Frequency, 
+            first.Character + second.Character, 
+            second, first);
+    }
+    
+}
--- greed-and-encryption/Program.cs
+++ greed-and-encryption/Program.cs
@@ -1,5 +1,7 @@
 ﻿Console.WriteLine(Directory.GetCurrentDirectory());
-// sorry, i didn't manage to make not absolute path work, so i use absolute, which is different for you
+// sorry, i didn't manage to make not absolute path work, so i use absolute,
+// which is different for you
+
 string variaPath = "C:/Users/chorn/Desktop/it/greed-and-encryption/greed-and-encryption/sherlock.txt";
 string[] lines = File.ReadAllLines(variaPath);
 Dictionary<char, int> frequencies = new Dictionary<char, int>();
@@ -18,7 +20,24 @@ foreach(var line in lines)
     }
 }
 
+PriorityQueue<KeyValuePair<char, int>, int> huffmanTree = new PriorityQueue<KeyValuePair<char, int>, int>();
+foreach (var element in frequencies)
+{
+    huffmanTree.Enqueue(element, element.Value);
+}
+
 foreach (var element in frequencies)
 {
     Console.WriteLine(element);
+}
+
+Console.WriteLine("           ");
+Console.WriteLine("           ");
+Console.WriteLine("           ");
+Console.WriteLine("           ");
+Console.WriteLine("           ");
+Console.WriteLine("           ");
+for (int i = 0; i < frequencies.Count; i++)
+{
+    Console.WriteLine(huffmanTree.Dequeue());
 }
