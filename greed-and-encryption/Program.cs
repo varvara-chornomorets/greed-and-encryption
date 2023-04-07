73e9a1e9b751a116a953517daaa6f717266655d1
varvara-chornomorets <vchornomorets@kse.org.ua>
2023-04-07 10:06:15 +0300

    created dictionary to counr frequencies of each char

--- greed-and-encryption/Program.cs
+++ greed-and-encryption/Program.cs
@@ -1,3 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿Console.WriteLine(Directory.GetCurrentDirectory());
+// sorry, i didn't manage to make not absolute path work, so i use absolute, which is different for you
+string variaPath = "C:/Users/chorn/Desktop/it/greed-and-encryption/greed-and-encryption/sherlock.txt";
+string[] lines = File.ReadAllLines(variaPath);
+Dictionary<char, int> frequencies = new Dictionary<char, int>();
+foreach(var line in lines)
+{
+    foreach (var c in line)
+    {
+        try
+        {
+            frequencies[c] += 1;
+        }
+        catch(Exception ex)
+        {
+            frequencies[c] = 1;
+        }
+    }
+}
 
-Console.WriteLine("Hello, World!");
+foreach (var element in frequencies)
+{
+    Console.WriteLine(element);
+}
